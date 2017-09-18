4e8b1a18aca61d8a2d27a6aea2567acf17c1899f
Roman Tokarev <rtokarev@corp.mail.ru>
2017-09-18 18:26:25 +0300

    Decrease arena used too

--- salloc.c
+++ salloc.c
@@ -606,6 +606,8 @@ sfree(void *ptr)
 				SLIST_REMOVE(&cache->arena->slabs, slab, slab, link);
 				assert(cache->arena->size >= SLAB_SIZE);
 				cache->arena->size -= SLAB_SIZE;
+				assert(cache->arena->used >= SLAB_SIZE);
+				cache->arena->used -= SLAB_SIZE;
 				r = munmap(slab, SLAB_SIZE);
 				slab_still_alive = false;
 			}
